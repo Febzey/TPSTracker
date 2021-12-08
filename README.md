3572e88b52790446a7387a369dc3c399920334b6
Febzey <braydent1235@gmail.com>
2021-12-08 14:37:05 -0600

    ww

---
+++ README.md
@@ -0,0 +1,7 @@
+# A Simple Minecraft Bot to track over all player count and average TPS
+
+Steps to using this bot.
+1. A mysql Database. I use MariaDB
+2. A Minecraft Java account
+3. nodejs 16.10.0 installed
+4. Common sense
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,62 @@
 import startBot from './createBot.js';
 import connect from './database.js';
 import TPS from './getTps.js';
+import chalk from 'chalk';
 
 (async () => {
 
     const database:any = await connect();
 
     if (!database) return () => {
-        console.error("Could not connect to the database");
+        console.error(chalk.red("Could not connect to the database"));
+        console.error(chalk.red("Exiting...."));
         return process.exit(1);
     }
+    console.log(chalk.green("Connection made to database successfully."));
 
-    console.log(database);
+
 
     const bot:any = await startBot();
-    if (!bot) return;
+    console.log(chalk.green("Mineflayer bot has logged in successfully."))
+
 
     bot.loadPlugin(TPS);
 
 
+    const logTps = async () => {
+       if (process.uptime() / 60 < 1) return;
+        console.log(bot.getTps(), " ", Date.now());
+        database.query('INSERT INTO TPS (tps, time) VALUES (?,?)', [parseInt(bot.getTps()), Date.now()], (err:unknown) => {
+            if (err) throw err;
+        });
+    };
 
-    console.log("Mineflayer bot has logged in successfully.")
 
-    bot.on("chat", (user,msg)=>{
+    setInterval(() => {
+        logTps();
+    },60000)
+
+    bot.on('end' || 'kicked', () => {
+        console.warn(chalk.yellow("Bot has ended. process will now exit."));
+        process.exit(0);
+    });
+
+    let cooldown = new Set();
+    bot.on("chat", (user: string, msg: string)=>{
         if(user === bot.username) return;
-        if(msg === 'tps') {
-            bot.chat(`${bot.getTps()}`)
+        if(msg === 'TPS'.toLocaleLowerCase()) {
+            
+            if (cooldown.has(bot.username)) {
+                bot.whisper(user, "Anti spam, wait 3 seconds.");
+                return setTimeout(() => { cooldown.delete(bot.username) }, 3000)
+            }
+
+            cooldown.add(bot.username);
+            
+            if (process.uptime() / 60 < 1) return bot.whisper(user, "TPS not calculated yet")
+            return bot.whisper(user, `${bot.getTps()}`);
+
         }
-    })
+    });
 
 })();   
